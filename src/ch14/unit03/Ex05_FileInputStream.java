55e7084bd8f041a150fcfd2fdbb19ed1d256088a
jiyoungg02 <jiyoung_g02@naver.com>
2025-05-09 15:06:07 +0900

    0509 InOutPutStream

---
+++ src/ch14/unit03/Ex05_FileInputStream.java
@@ -0,0 +1,25 @@
+package ch14.unit03;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class Ex05_FileInputStream {
+	public static void main(String[] args) {
+		String pathname = "test.txt";
+		int data;
+		
+		try (FileInputStream fis = new FileInputStream(pathname)){
+			System.out.println(pathname + " 파일 내용...");
+			
+			while((data = fis.read()) != -1) {
+				System.out.write(data);
+			}
+			System.out.flush();
+			
+		} catch (FileNotFoundException e) {
+			System.out.println("파일이 존재하지 않습니다.");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
