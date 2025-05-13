eeeb2ea46d1cf4954c96d10c891de74141801b76
jiyoungg02 <jiyoung_g02@naver.com>
2025-05-13 13:48:41 +0900

    0513 commit ObjectInOutputStream

---
+++ src/ch14/unit03/score/App.java
@@ -0,0 +1,7 @@
+package ch14.unit03.score;
+
+public class App {
+	public static void main(String[] args) {
+		new ScoreUI().menu();
+	}
+}
---
+++ src/ch14/unit04/score/App.java
@@ -0,0 +1,7 @@
+package ch14.unit04.score;
+
+public class App {
+	public static void main(String[] args) {
+		new ScoreUI().menu();
+	}
+}
